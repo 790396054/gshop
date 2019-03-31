e5a99611b4f63431a8afc055690c180089253b06
mmg <miaomiaogong@gmail.com>
2019-03-31 15:35:48 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-view/>
+    <FooterGuide/>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    components: {
+      FooterGuide
+    }
+
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .app
+    color: red
+</style>
---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    FooterGuide
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FooterGuide'
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
