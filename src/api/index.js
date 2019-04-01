b71fb0a095962a761258d376feea9f41f9a0f9ea
mmg <miaomiaogong@gmail.com>
2019-04-01 22:38:18 +0800

    完成异步请求

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,9 @@
 函数的返回值: promise对象
  */
 
+// const BASE_URL = 'http://localhost:4000'
+const BASE_URL = '/api'
+
 import ajax from './ajax'
 
 // 1、根据经纬度获取位置详情
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  //  代理相关配置
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    proxy: {
+      '/api': { // 匹配所有以 '/api'开头的请求路径
+        target: 'http://localhost:4000', // 代理目标的基础路径
+        changeOrigin: true, // 支持跨域
+        pathRewrite: {// 重写路径: 去掉路径中开头的'/api'
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
